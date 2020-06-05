45a472289d5fc94b307e514ff927dc3895a04794
sharadroy-choudhury <SharadRoy.Choudhury@mheducation.com>
2020-06-05 03:24:43 +0100

    signup working

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import axios from 'axios';
+const API_URL="http://localhost:8080"
 class signUp extends Component {
     constructor() {
         super();
 
         this.state = {
-            email: '',
+            emailId: '',
             password: '',
-            name: '',
+            fullName: '',
             hasAgreed: false
         };
-
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -33,13 +34,46 @@ class signUp extends Component {
         console.log(this.state);
     }
 
+
+
+
+
+   signUpAction = e => {
+    e.preventDefault();
+console.log("submitted");
+axios.post('http://localhost:8080/testjpa_war_exploded/api/register', this.state).then(response => {
+if(response.data!=null)
+{
+    alert("Hello "+this.state.fullName +"!" + "You are now registered on our portal");
+    alert("Please sign in with your credentials");
+}
+else
+{
+alert("User not Registed:Error");
+
+
+}
+})
+.catch(error =>{
+    console.log(error);
+    alert(error);
+})
+
+
+
+}
+
+
+        
+    
+
     render() {
         return (
         <div className="FormCenter">
-            <form onSubmit={this.handleSubmit} className="FormFields">
+            <form  onSubmit={this.signUpAction}  className="FormFields">
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="name">Full Name</label>
-                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" value={this.state.name} onChange={this.handleChange} />
+                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="fullName" value={this.state.fullName} onChange={this.handleChange} />
               </div>
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="password">Password</label>
@@ -47,7 +81,7 @@ class signUp extends Component {
               </div>
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="emailId" value={this.state.emailId} onChange={this.handleChange} />
               </div>
 
               <div className="FormField">
@@ -57,7 +91,11 @@ class signUp extends Component {
               </div>
 
               <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign Up</button> <Link to="/sign-in" className="FormField__Link">I'm already member</Link>
+                  <button className="FormField__Button mr-20"  type="submit" >Sign Up</button> 
+                  
+                  
+                  
+                  <Link to="/sign-in" className="FormField__Link">I'm already member</Link>
               </div>
             </form>
           </div>
