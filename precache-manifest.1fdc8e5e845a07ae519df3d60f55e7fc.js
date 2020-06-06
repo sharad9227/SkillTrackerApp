ab4074748a52be76d9631330e08ac563115fab4a
sharadroy-choudhury <SharadRoy.Choudhury@mheducation.com>
2020-06-06 14:38:30 +0100

    Updates

---
+++ precache-manifest.1fdc8e5e845a07ae519df3d60f55e7fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f51c6460daa67b493de4e6a2884497",
+    "url": "/SkillTrackerApp/index.html"
+  },
+  {
+    "revision": "c514c8ff6f4747b8c46e",
+    "url": "/SkillTrackerApp/static/css/main.94635ba8.chunk.css"
+  },
+  {
+    "revision": "968d45710ef3d23e5dee",
+    "url": "/SkillTrackerApp/static/js/2.b275e880.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/SkillTrackerApp/static/js/2.b275e880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c514c8ff6f4747b8c46e",
+    "url": "/SkillTrackerApp/static/js/main.c8d35f8f.chunk.js"
+  },
+  {
+    "revision": "e173b152960a0f70886c",
+    "url": "/SkillTrackerApp/static/js/runtime-main.a0d03c9c.js"
+  },
+  {
+    "revision": "8659c389a98e2fea64e603c5a0e3cfc1",
+    "url": "/SkillTrackerApp/static/media/img1.8659c389.png"
+  }
+]);
